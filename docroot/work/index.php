029e8edcbe456bdd69f53b6d841acbac5ab766a7
Wes Jones <waj194@gmail.com>
2015-09-12 22:53:50 -0400

    Add work section with timeline.

---
+++ docroot/work/index.php
@@ -0,0 +1,297 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Work | Drupal Developer Rochester NY | WesJones.net</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles -->
+  <link href="/css/style.css" rel="stylesheet">
+
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+</head>
+
+<body class="work">
+
+  <div class="container">
+
+    <!-- Static navbar -->
+    <nav class="navbar navbar-default" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="/">WesJones.net</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li class="active"><a href="/work">Work <span class="sr-only">(current)</span></a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="mailto:wes@wesjones.net">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div><!--/.container-fluid -->
+    </nav>
+
+    <div class="jumbotron container">
+      <!-- Main -->
+      <div class="col-xs-9 col-sm-8 col-md-7">
+        <h1>Work</h1>
+        <p>View Wes Jones' work in <strong>Web design</strong>, <strong>Drupal development</strong>, and <strong>PHP/MySQL application development</strong>.</p>
+      </div>
+    </div>
+
+    <div class="main-content">
+
+      <p><a class="btn btn-primary" href="/files/Resume-WesJones.pdf">View Wes' full resume &raquo;</a></p>
+      
+      <h2>Timeline</h2>
+
+      <div class="container">
+        <div class="page-header">
+            <h1 id="timeline">Timeline</h1>
+        </div>
+        <ul class="timeline">
+            <li>
+              <div class="timeline-badge job"><i class="glyphicon glyphicon-briefcase"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">Software Engineer at Third &amp; Grove</h4>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-calendar"></i> Feb 2015 - present</small></p>
+                </div>
+                <div class="timeline-body">
+                  <ul>
+                    <li>http://www.magmutual.com/</li>
+                    <li>https://www.fastly.com/</li>
+                    <li>Novartis</li>
+                    <li>http://www.linguisticsociety.org/</li>
+                    <li>https://www.mint.com/</li>
+                    <li>http://www.quicken.com/</li>
+                  </ul>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="timeline-badge job"><i class="glyphicon glyphicon-briefcase"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">Freelance Drupal Developer</h4>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-calendar"></i> Dec 2011 - present</small></p>
+                </div>
+                <div class="timeline-body">
+                  <p>Mussum ipsum</p>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-badge project"><i class="glyphicon glyphicon-cog"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">George Eastman House - Technicolor100</h4>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-link"></i> http://eastman.org/technicolor</small></p>
+                </div>
+                <div class="timeline-body">
+                  <p>Missum ipsum</p>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-badge project"><i class="glyphicon glyphicon-cog"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">Bayer Video Tours</h4>
+                  <ul>
+                    <li>http://www.bayervideotours.com/</li>
+                  </ul>
+                </div>
+                <div class="timeline-body">
+                  <p>Missum ipsum</p>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-badge project"><i class="glyphicon glyphicon-cog"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">Museum of Science</h4>
+                  <ul>
+                    <li>http://www.mos.org/</li>
+                    <li>http://livinglab.org/</li>
+                    <li>http://nanomedicine-explorer.net/</li>
+                  </ul>
+                </div>
+                <div class="timeline-body">
+                  <p>Missum ipsum</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="timeline-badge job"><i class="glyphicon glyphicon-briefcase"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">SEO Specialist / Web Developer, Accelerate Media</h4>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-calendar"></i> Sept 2014 - Jan 2015</small></p>
+                </div>
+                <div class="timeline-body">
+                  <ul>
+                    <li>http://www.draftsharks.com/</li>
+                    <li>http://www.monroetractor.com/</li>
+                    <li>All Collector Cars</li>
+                    <li>IASSIDD</li>
+                  </ul>
+                  <p>Mussum ipsum</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="timeline-badge job"><i class="glyphicon glyphicon-briefcase"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">Web Technologies Specialist, The Strong</h4>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-calendar"></i> May 2009 - Dec 2011</small></p>
+                </div>
+                <div class="timeline-body">
+                  <p>Mussum ipsum</p>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-badge project"><i class="glyphicon glyphicon-cog"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">The Strong</h4>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-link"></i> http://www.thestrong.org/</small></p>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-link"></i> http://www.museumofplay.org/</small></p>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-link"></i> http://www.journalofplay.org/</small></p>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-link"></i> http://www.icheg.org/</small></p>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-link"></i> http://www.toyhalloffame.org/</small></p>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-link"></i> http://www.libraryandarchivesofplay.org/</small></p>
+                </div>
+                <div class="timeline-body">
+                  <p>Missum ipsum</p>
+                </div>
+              </div>
+            </li>
+            <li class="timeline-inverted">
+              <div class="timeline-badge project"><i class="glyphicon glyphicon-cog"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">The Strong Online Collections</h4>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-link"></i> http://www.thestrong.org/online-collections/</small></p>
+                </div>
+                <div class="timeline-body">
+                  <p>Missum ipsum</p>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div class="timeline-badge job"><i class="glyphicon glyphicon-briefcase"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">Student Web Programmer, The College at Brockport</h4>
+                  <p><small class="text-muted"><i class="glyphicon glyphicon-calendar"></i> May 2008 - May 2009</small></p>
+                </div>
+                <div class="timeline-body">
+                  <p>http://www.brockport.edu/</p>
+                </div>
+              </div>
+            </li>
+        </ul>
+    </div>
+
+    <hr />
+
+    <div class="row hero-panel">
+
+      <div class="col-md-4 clearfix hero-feature">
+        <div class="col-xs-3 col-md-12 img-container">
+          <img src="/img/colosseum-icon.png" alt="Drupal icon">
+        </div>
+        <div class="col-xs-9 col-md-12 content-container">
+          <h3 class="">Museum Specialist</h3>
+          <p>Wes has over 5 years of experience working closely with museums on website redesigns and IMLS grant-funded projects.</p>
+        </div>
+      </div>
+
+      <div class="col-md-4 clearfix hero-feature">
+        <div class="col-xs-3 col-md-12 img-container">
+          <img src="/img/druplicon-small.png" alt="Drupal icon">
+        </div>
+        <div class="col-xs-9 col-md-12 content-container">
+          <h3 class="">Drupal Consulting</h3>
+          <p>Wes specializes in site building, theming and custom module development using the Drupal 7 web application framework.</p>
+        </div>
+      </div>
+
+      <div class="col-md-4 clearfix hero-feature">
+        <div class="col-xs-3 col-md-12 img-container">
+          <img src="/img/html5-logo-small.png" alt="HTML5 logo">
+        </div>
+        <div class="col-xs-9 col-md-12 content-container">
+          <h3>Website Development</h3>
+          <p>Wes provides custom PHP website solutions using modern frameworks and industry best practices.</p>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <div class="col-xs-12 text-center">
+        <p>
+          <a class="btn btn-lg btn-primary" href="mailto:wes@wesjones.net" role="button">Contact Wes for your next project &raquo;</a>
+        </p>
+      </div>
+
+    </div>
+
+    <hr />
+    <!-- Footer -->
+    <footer>
+      <div class="row text-right">
+        <div class="col-lg-12">
+          <p>Copyright &copy; WesJones.net 2014</p>
+        </div>
+      </div>
+    </footer>
+
+  </div> <!-- /container -->
+
+  <!-- Bootstrap core JavaScript
+  ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="/bootstrap/js/bootstrap.min.js"></script>
+
+  <?php if ($_SERVER['HTTP_HOST'] == 'www.wesjones.net'): ?>
+    <!-- Google Analytics -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-28900790-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+  <?php endif; ?>
+</body>
+</html>
