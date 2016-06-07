7ffd035876caa4b433bb58a55027740d3ba572fb
Wes Jones <waj194@gmail.com>
2016-06-07 16:42:53 -0500

    Add first draft of Spanish translation.

--- docroot/_translations/es.php
+++ docroot/_translations/es.php
@@ -1,1 +1,15 @@
-es.php
+<?php
+$translate['home_pagetitle'] = 'Desarrollo de páginas web multilingües | Drupal consultante Cuenca Ecuador';
+$translate['nav_home'] = 'Inicio';
+$translate['nav_about'] = 'Acerca de';
+$translate['nav_work'] = 'Trabajo';
+$translate['nav_contact'] = 'Contacto';
+$translate['jumbotron_h1'] = 'Ofrece sitios web de Drupal y servicios de traducción.';
+$translate['jumbotron_p'] = 'Wes Jones es un desarollador de <strong>Rochester, Nueva York</strong> que vive en Cuenca, Ecuador. Él se especializa en <strong>Drupal</strong> y Backdrop CMS, y desarollo de páginas web multilingües en <strong>español</strong> y <strong>chino mandarín</strong> también.';
+$translate['cta_button_text'] = 'Pónganse en contacto con Wes para su próximo proyecto';
+$translate['feature1_h1'] = 'Experto multilingües';
+$translate['feature1_p'] = 'Wes se habla español y chino mandarín con fluidez, y tiene la experiencia en la creación de sitios web multilingües, utilizando el sistema Drupal.';
+$translate['feature2_h1'] = 'Drupal consultante';
+$translate['feature2_p'] = 'We se especialista en la creación de sitios web, tematización y desarollo de los módulos personalizados, utilizando el sistema de aplicaciones web de Drupal 7.';
+$translate['feature3_h1'] = 'Desarollo de sitios web';
+$translate['feature3_p'] = 'Wes ofrece soluciones de sitio web PHP personalizadas utilizando sistemas modernas y las mejores prácticas de la industria.';
