12ad7103ba4a6def7d1074aa119ce4bab1f93377
Wes Jones <waj194@gmail.com>
2016-06-07 16:52:33 -0500

    Apply Spanish translation to homepage.

--- docroot/_includes/global.php
+++ docroot/_includes/global.php
@@ -1,5 +1,31 @@
 <?php
-function t($variable, $default) {
-  $lang = 'en';
+function dpm($var) {
+  print "<pre>";
+  print_r($var);
+  print "</pre>";
+}
+
+function get_lang() {
+  $lang_list = array('en', 'es', 'zh');
+  if (!empty($_GET['lang'])) {
+    if (in_array($_GET['lang'], $lang_list)) {
+      return $_GET['lang'];
+    }
+  }
+  return 'en';
+}
+
+function t($key, $default) {
+  $lang = get_lang();
+
+  $translation_filepath = dirname(__FILE__) . '/../_translations/' . $lang . '.php';
+  if (file_exists($translation_filepath)) {
+    global $translate;
+    require_once $translation_filepath;
+    if (!empty($translate[$key])) {
+      return $translate[$key];
+    }
+  }
+
   return $default;
 }
--- docroot/_partials/contact_cta.php
+++ docroot/_partials/contact_cta.php
@@ -2,7 +2,7 @@
 
   <div class="col-xs-12 text-center">
     <p>
-      <a class="btn btn-lg btn-primary" href="/contact" role="button">Contact Wes for your next project &raquo;</a>
+      <a class="btn btn-lg btn-primary" href="/contact" role="button"><?php print t('cta_button_text', 'Contact Wes for your next project'); ?> &raquo;</a>
     </p>
   </div>
 
