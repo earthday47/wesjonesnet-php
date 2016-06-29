214aabc70cf6580722cf478e9bbb642f69b7349c
Wes Jones <waj194@gmail.com>
2016-06-29 14:15:40 -0500

    Update Spanish translation.

--- docroot/_translations/es.php
+++ docroot/_translations/es.php
@@ -1,50 +1,50 @@
 <?php
-$translate['home_pagetitle'] = 'Desarrollo de páginas web multilingües | Drupal consultante Cuenca Ecuador';
+$translate['home_pagetitle'] = 'Desarrollo de páginas web multilingües | Consultor Drupal, Cuenca-Ecuador';
 $translate['nav_home'] = 'Inicio';
 $translate['nav_about'] = 'Acerca de Wes';
 $translate['nav_work'] = 'Trabajo';
 $translate['nav_contact'] = 'Contacto';
 $translate['jumbotron_h1'] = 'Ofrece sitios web de Drupal y servicios de traducción.';
-$translate['jumbotron_p'] = 'Wes Jones es un desarollador de <strong>Rochester, Nueva York</strong> que vive en Cuenca, Ecuador. Él se especializa en <strong>Drupal</strong> y Backdrop CMS, y desarollo de páginas web multilingües en <strong>español</strong> y <strong>chino mandarín</strong> también.';
-$translate['cta_button_text'] = 'Contacta Wes para su próximo proyecto';
-$translate['feature1_h1'] = 'Experto multilingües';
-$translate['feature1_p'] = 'Wes se habla español y chino mandarín con fluidez, y tiene la experiencia en la creación de sitios web multilingües, utilizando el sistema Drupal.';
-$translate['feature2_h1'] = 'Drupal consultante';
-$translate['feature2_p'] = 'We se especialista en la creación de sitios web, tematización y desarollo de los módulos personalizados, utilizando el sistema de aplicaciones web de Drupal 7.';
+$translate['jumbotron_p'] = 'Wes Jones es un desarollador nativo de <strong>Rochester, Nueva York</strong> que en la actualidad vive en Cuenca, Ecuador. Se especializa en <strong>Drupal</strong>, <strong>Backdrop CMS</strong>, y desarollo de páginas web multilingües en <strong>español</strong> y <strong>chino mandarín</strong> también.';
+$translate['cta_button_text'] = 'Contacte a Wes<span class="hidden-xs"> para su próximo proyecto</span>';
+$translate['feature1_h1'] = 'Experto multilingüe';
+$translate['feature1_p'] = 'Wes habla español y chino mandarín con fluidez, además tiene experiencia en la creación de sitios web multilingües utilizando el sistema Drupal.';
+$translate['feature2_h1'] = 'Consoltoria Drupal';
+$translate['feature2_p'] = 'Wes se especializa en la creación de sitios web, tematización y desarollo de módulos personalizados, utilizando el sistema de aplicaciones web de Drupal 7.';
 $translate['feature3_h1'] = 'Desarollo de sitios web';
-$translate['feature3_p'] = 'Wes ofrece soluciones de sitio web PHP personalizadas utilizando sistemas modernas y las mejores prácticas de la industria.';
+$translate['feature3_p'] = 'Wes ofrece soluciones de sitio web PHP personalizadas utilizando sistemas modernos y así como las mejores prácticas de la industria.';
 
-$translate['about_pagetitle'] = 'Acerca de Wes | Desarollador de Drupal Cuenca Ecuador';
+$translate['about_pagetitle'] = 'Acerca de Wes | Desarollador de Drupal, Cuenca-Ecuador';
 $translate['about_h1'] = 'Acerca de Wes';
-$translate['about_h2'] = 'Wes Jones es un desarollador web y consultante de Drupal de Rochester, Nueva York.';
+$translate['about_h2'] = 'Wes Jones es desarollador web y consultor de Drupal nativo de Rochester, Nueva York.';
 $translate['resume_link'] = 'Ve el currículum completo de Wes';
 $translate['about_body'] = "
-<h2>Drupal consultante</h2>
+<h2>Consultoria Drupal</h2>
 
-<p>Wes completó su primer proyecto con Drupal para el Strong National Museum of Play en 2009&mdash;Una implementación de Drupal 6 con 6 sitios entralazándose, parte de un proyecto de 18 meses que incluye un cambio de marco y rediseño del sitio web. Desde entonces, Wes ha desplegado dieces de sitios con Drupal 6 y Drupal 7. Ha utilizando a su amplio conocimiento de la Drupal API para construir las temas de Drupal y desarollar los módulos personalizados.</p>
+<p>Wes completó su primer proyecto con Drupal para el Strong National Museum of Play en 2009: una implementación de Drupal 6 con 6 sitios entrelazados, parte de un proyecto de 18 meses que incluyó un cambio de marco y rediseño del sitio web. Desde entonces, Wes ha desplegado más de una docena de sitios con Drupal 6 y Drupal 7. Ha utilizando su amplio conocimiento de la Drupal API para construir temas de Drupal y desarollar módulos personalizados.</p>
 
 <h2>Desarollo de sitios web</h2>
 
-<p>Wes se especializa en el desarollo de sitios web personalizados y aplicaciones web en el pilo LAMP&mdash;Linux, Apache MySQL, PHP&mdahs;utilizando PHP, HTML, CSS, y Javascript (jQuery).</p>
+<p>Wes se especializa en el desarollo de sitios web personalizados y aplicaciones web en LAMP: Linux, Apache MySQL, PHP; utilizando PHP, HTML, CSS, y Javascript (jQuery).</p>
 
-<p>Aunque Wes tiene lo más experiencia trabanjando con Drupal, él ha construido sitios web utilizando PHP personalizado, Wordpress, y sistemas MVC como Yii.</p>
+<p>Aunque la mayor parte de la experiencia de Wes ha sido con Drupal, él ha construido sitios web utilizando PHP personalizado, Wordpress, y sistemas MVC como Yii Framework.</p>
 
 <h2>他也会讲中文!</h2>
 
-<p>Wes le encanta aprender los lenguajes. Se habla chino mandarín y español con fluidez y además ha estudiado cantonés, coreano, japonés, y el lenguaje de señas americano (ASL).</p>
+<p>A Wes le encanta aprender idiomas. El habla chino mandarín y español con fluidez y además ha estudiado cantonés, coreano, japonés, y el lenguaje de señas americano (ASL).</p>
 
-<p>Wes vivió en China a partir de 2012-2014 y viajó extensamente a través de Asia. Cuando esté en casa, le gusta tocar la música y mirar deportes, en particular el fútbol europeo.</p>
+<p>Wes vivió en China entre 2012 y 2014 y viajó extensamente por toda Asia. En sus ratos libres en casa, le gusta tocar música y ver deportes, en particular el fútbol europeo.</p>
 ";
 
 $translate['work_pagetitle'] = 'Trabajo | Desarollador Drupal Cuenca Ecuador';
 $translate['work_h1'] = 'Trabajo';
-$translate['work_p'] = 'Ve el trabajo de Wes Jones en <strong>diseño web</strong>, desarollo Drupal, y el desarollo de <strong>aplicaciones de PHP y MySQL</strong>.';
+$translate['work_p'] = 'Vea el trabajo de Wes Jones en <strong>diseño web</strong>, desarollo Drupal, y desarollo de <strong>aplicaciones de PHP y MySQL</strong>.';
 $translate['work_title'] = 'Línea de tiempo';
 
-$translate['contact_pagetitle'] = 'Contacto | Consultante Drupal y desarollo web';
+$translate['contact_pagetitle'] = 'Contacto | Consultoria Drupal y desarollo web';
 $translate['contact_body1'] = "
-<h3>Si desea ponerse en contacto conmigo sobre el desarrollo personalizado, un proyecto del sitio web, o los servicios del traducción, envíame un email a <a href=\"mailto:wes@wesjones.net\">wes@wesjones.net</a>.</h3>
+<h3>Hola! Si desea ponerse en contacto conmigo para desarrollo personalizado, algún proyecto de sitios web, o los servicios de traducción, puede hacerlo al correo electrónico: <a href=\"mailto:wes@wesjones.net\">wes@wesjones.net</a>.</h3>
 
-<h3>O alcancarme en una de las siguientes redes:<br /><br /></h3>
+<h3>O encuéntreme en una de las siguientes redes:<br /><br /></h3>
 ";
-$translate['contact_body2'] = '<h3>Espero con interés hablar con usted!</h3>';
+$translate['contact_body2'] = '<h3>A la espera de su contacto!</h3>';
