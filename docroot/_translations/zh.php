d8f952678b3c15c47e61dc36342e88f28f26a352
Wes Jones <waj194@gmail.com>
2016-07-04 22:24:11 -0500

    Add ZH about body translation, and revise ZH translation.

--- docroot/_translations/zh.php
+++ docroot/_translations/zh.php
@@ -8,29 +8,42 @@ $translate['jumbotron_h1'] = 'Drupal网站和翻译服务。';
 $translate['jumbotron_p'] = 'Wes Jones是位专业的网站开发人员。他来自<strong>Rochester NY（美国纽约州罗切斯特市）</strong>现在住Cuenca Ecuador（厄瓜多尔周昆卡市）。他专业做网站建设跟<strong>Drupal</strong>和<strong>Backdrop CMS</strong>，另外做多种语言网站建设，用<strong>西班牙语</strong>和<strong>中文（普通话）</strong>。';
 $translate['cta_button_text'] = '联系Wes<span class="hidden-xs">为了您的下一个工程</span>';
 $translate['feature1_h1'] = '多种语言庄稼';
-$translate['feature1_p'] = 'Wes会流利地说西班牙语和普通话，以及有经验用Drupal机架开发多种语言网站。';
+$translate['feature1_p'] = 'Wes会流利地说英文，西班牙语，和普通话，而且有经验用Drupal机架开发多种语言网站。';
 $translate['feature2_h1'] = 'Drupal咨询';
-$translate['feature2_p'] = 'Wes是个专家，利用Drupal 7网站应用程序框架来做网站建设，主题化以及定制module开发。';
+$translate['feature2_p'] = 'Wes是位专家，利用Drupal 7网站应用程序框架来做网站建设，主题化以及定制模块开发。';
 $translate['feature3_h1'] = '网站开发';
 $translate['feature3_p'] = 'Wes利用现代的框架和行业最佳实践提供定制PHP网站解决方案。';
 
 $translate['about_pagetitle'] = '关于 ｜ Drupal开发人员 Cuenca Ecuador（厄瓜多尔周昆卡市）';
 $translate['about_h1'] = '关于Wes';
-$translate['about_h2'] = 'Wes Jones是个网站开发人员和Drupal顾问，来自Rochester&nbsp;NY (美国纽约州罗切斯特市)。';
-$translate['about_p'] = 'Wes爱他的工作，促动他的是对科技，数学，和解决问题的热心。';
+$translate['about_h2'] = 'Wes Jones是位网站开发人员与Drupal咨询师，来自Rochester&nbsp;NY (美国纽约州罗切斯特市)。';
+$translate['about_p'] = 'Wes爱他的工作，促动他的是他对科技，数学，和解决问题的热心。';
 $translate['resume_link'] = '请看Wes的简历（英文）';
-//$translate['about_body'] = "";
+$translate['about_body'] = "<h2>Drupal咨询</h2>
+
+<p>在2009年，Wes完成了他第一个Drupal工程，用Drupal 6做六个连锁在一起的网站，是长十八个月的品牌优化与重新设计网站工程的一部分。从那时起，他用了Drupal 6或Drupal 7部署了十几个网站。他用自己对Drupal API的丰富知识建设Drupal的主题和定制模块。</p>
+
+<h2>网站开发</h2>
+
+<p>Wes专一开发定制网站和网站应用程序，用LAMP的结构，就是Linux，Apache，MySQL，PHP；也应用PHP，HTML，CSS，和Javascript（jQuery）来编程。</p>
+
+<p>虽然Wes有更多经历用Drupal结构，他会用定制PHP，Wordpress，或Yii Framework一类的MVC结构建设网站。</p>
+
+<h2>Él se habla español también!</h2>
+
+<p>Wes爱学习外语。他会流利地说英文，普通话，西班牙语，而且有学过广东话，韩语，日语，美国手语（ASL）。</p>
+
+<p>Wes从2012到2014住在中国，也周游很多国家。当他在家，他喜欢奏乐和看球赛，特别欧洲足球。</p>";
 
 $translate['work_pagetitle'] = '工作 ｜ Drupal开发人员 Cuenca Ecuador（厄瓜多尔周昆卡市）';
 $translate['work_h1'] = '工作';
-$translate['work_p'] = '请看Wes Jones关于<strong>网页设计</strong>，<strong>Drupal开发</strong>，和<strong>PHP/MySQL应用程序开发</strong>的工作。';
+$translate['work_p'] = '请看Wes Jones关于<strong>网页设计</strong>，<strong>Drupal网站开发</strong>，和<strong>PHP/MySQL应用程序开发</strong>的工作。';
 $translate['work_title'] = '工作经历';
 
 $translate['contact_pagetitle'] = '联系 ｜ Drupal咨询和网站开发';
 $translate['contact_h1'] = '联系';
 $translate['contact_body1'] = "
-<h3>
-如果您想与我联系关于定制开发，您的网页工程，或翻译服务，用<a href=\"mailto:wes@wesjones.net\">wes@wesjones.net</a>发电子邮件给我。</h3>
+<h3>您好！如果您想与我联系关于定制开发，您的网页工程，或翻译服务，请用<a href=\"mailto:wes@wesjones.net\">wes@wesjones.net</a>发电子邮件给我。</h3>
 
 <h3>还是用一下的网络来与我联系：<br /><br /></h3>
 ";
